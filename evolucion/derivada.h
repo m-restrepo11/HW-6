019e64177fd4b93ec42ca399f4f64dafe9dd1df4
Matt <systemmanager@fiscomputo.(none)>
2013-11-07 13:31:27 -0500

    Más eliminación

--- evolucion/derivada.h
+++
@@ -1,6 +0,0 @@
-double x_punto(double t,double x, double v_x,  double y,double v_y);
-double y_punto(double t,double x, double v_x,  double y,double v_y);
-double v_x_punto(double t,double x, double v_x,  double y,double v_y);
-double v_y_punto(double t,double x, double v_x,  double y,double v_y);
-
-
--- evolucion/runge_kutta4.h
+++
@@ -1,3 +0,0 @@
-double funcionVel(float t, float x,float v);
-double funcionAce(float t,  float v,float g,float m,float r3);
-double rungekutta(float t,  float v,float g,float m,float r3);
